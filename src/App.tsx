6fc9cc214e7c826aa167d9799e96f03d3dcef06e
Eugene <kulinich.eugene.2412@gmail.com>
2023-08-28 23:46:32 +0600

    add about company page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import './App.scss';
-import { AboutPage } from './pages/AboutPage/AboutPage';
+import { AboutCompany } from './pages/AboutCompanyPage/AboutCompanyPage';
 import { CatalogPage } from './pages/CatalogPage/CatalogPage';
 import { Footer } from './components/Footer/Footer';
 import { Header } from './components/Header/Header';
 import { HomePage } from './pages/HomePage/HomePage';
-import { LoginPage } from './pages/LoginPage/LoginPage';
 import { Location } from './pages/LocationPage/LocationPage';
+import { LoginPage } from './pages/LoginPage/LoginPage';
 import { NotFoundPage } from './pages/NotFoundPage/NotFoundPage';
 import { ReviewsPage } from './pages/ReviewsPage/ReviewsPage';
 import { Route, Routes } from 'react-router-dom';
@@ -18,7 +18,7 @@ function App() {
       <Routes>
         <Route index element={<HomePage />} />
         <Route path="login" element={<LoginPage />} />
-        <Route path="about" element={<AboutPage />} />
+        <Route path="about" element={<AboutCompany />} />
         <Route path="reviews" element={<ReviewsPage />} />
         <Route path="catalog" element={<CatalogPage />} />
         <Route path="location" element={<Location />} />
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,10 +1,12 @@
 import './HomePage.scss';
+import { AboutCompany } from '../AboutCompanyPage/AboutCompanyPage';
 import { Location } from '../LocationPage/LocationPage';
 
 export const HomePage = () => {
   return (
     <>
       <Location />
+      <AboutCompany />
     </>
   );
 };
