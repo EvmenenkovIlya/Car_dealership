3f35c615e2685eb5271cd6802e1c73f495aa3b7c
Ilya <Evmenya@gmail.com>
2023-08-30 23:44:20 +0300

    add tabcontol styles

--- src/pages/ComparisonPage/ComparisonPage.tsx
+++ src/pages/ComparisonPage/ComparisonPage.tsx
@@ -1,11 +1,15 @@
 import './ComparisonPage.scss';
+import { TabControl } from '../../components/TabControl/TabControl';
 
 export const ComparisonPage = () => {
   // сделать route позже отдельным компонентом
   return (
     <section id="comparison">
       <div className="route">Главная {'>'} Сравнение</div>
-      <h1>Сравнение</h1>
+      <div>
+        <h1>Сравнение</h1>
+        <TabControl />
+      </div>
     </section>
   );
 };
