6414bc6f4fa2cc7620c4313fa9d2925aa377fea4
Eugene <kulinich.eugene.2412@gmail.com>
2023-09-04 01:04:40 +0600

    add Promotion section

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -3,6 +3,8 @@ import { AboutCompany } from '../AboutCompanyPage/AboutCompanyPage';
 import { AboutTabControl } from './components/AboutTabControl/AboutTabControl';
 import { Location } from '../LocationPage/LocationPage';
 import { QuickSelectionPage } from '../QuickSelectionPage/QuickSelectionPage';
+import { Promotion } from '../../components/Promotion/Promotion';
+import { discountPromotion } from '../../components/Promotion/models';
 
 export const HomePage = () => {
   return (
@@ -10,6 +12,7 @@ export const HomePage = () => {
       <QuickSelectionPage />
       <AboutCompany />
       <AboutTabControl />
+      <Promotion img={discountPromotion.img} header={discountPromotion.header} text={discountPromotion.text} />
       <Location />
     </>
   );
