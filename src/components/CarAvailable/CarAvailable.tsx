22167656f7f427c8fe8e7def734ae9ceab6aafd1
DariaKostrik <dolorez0927@yandex.ru>
2023-09-05 01:18:22 +0300

    create props for IndividualCarContainer

--- src/components/CarAvailable/CarAvailable.tsx
+++ src/components/CarAvailable/CarAvailable.tsx
@@ -1,415 +1,148 @@
+import '../IndividualCarContainer/IndividualCarContainer.scss';
 import './CarAvailable.scss';
+import { IndividualCarContainer } from '../IndividualCarContainer/IndividualCarContainer';
 import React from 'react';
-import blackButton from './assets/black-button.svg';
-import car from './assets/car.png';
-import fonCity from './assets/fon-city.png';
-import gasStation from './assets/gas-station.png';
-import greyButton from './assets/grey-button.svg';
-import horsePower from './assets/horse-power.png';
-import layer from './assets/layer.png';
-import overclocking from './assets/overclocking.png';
-import present from './assets/present.svg';
-import redButton from './assets/red-button.svg';
-import redHeart from './assets/red-heart.png';
-import redSpace from './assets/red-space.svg';
-import speed from './assets/speed.png';
-import whiteHeart from './assets/white-heart.png';
+import carSpecial from './assets/car.png';
+
+interface Cars {
+  id: number;
+  model: string;
+  type: string;
+  photo: string;
+  profit: string;
+  gift1: string;
+  gift2: string;
+  gift3: string;
+  price: string;
+  credit: string;
+  horsePower: string;
+  gasStation: string;
+  speed: string;
+  overclocking: string;
+}
+
+const carAvailable: Cars[] = [
+  {
+    id: 1,
+    type: 'Skoda Octavia',
+    model: '1.6 MPI MT Active',
+    photo: carSpecial,
+    profit: 'до 300 000 ₽',
+    gift1: 'Оборудование',
+    gift2: 'КАСКО',
+    gift3: 'Комплект резины',
+    price: 'от 1 615 000 ₽',
+    credit: 'от 115 000 ₽/мес.',
+    horsePower: ' 115 л.с',
+    gasStation: '5.3 л/км',
+    speed: '189 км/ч',
+    overclocking: '10,3 c.',
+  },
+  {
+    id: 2,
+    type: 'Skoda Octavia',
+    model: '1.6 MPI MT Active',
+    photo: carSpecial,
+    profit: 'до 300 000 ₽',
+    gift1: 'Оборудование',
+    gift2: 'КАСКО',
+    gift3: 'Комплект резины',
+    price: 'от 1 615 000 ₽',
+    credit: 'от 115 000 ₽/мес.',
+    horsePower: ' 115 л.с',
+    gasStation: '5.3 л/км',
+    speed: '189 км/ч',
+    overclocking: '10,3 c.',
+  },
+  {
+    id: 3,
+    type: 'Skoda Octavia',
+    model: '1.6 MPI MT Active',
+    photo: carSpecial,
+    profit: 'до 300 000 ₽',
+    gift1: 'Оборудование',
+    gift2: 'КАСКО',
+    gift3: 'Комплект резины',
+    price: 'от 1 615 000 ₽',
+    credit: 'от 115 000 ₽/мес.',
+    horsePower: ' 115 л.с',
+    gasStation: '5.3 л/км',
+    speed: '189 км/ч',
+    overclocking: '10,3 c.',
+  },
+  {
+    id: 4,
+    type: 'Skoda Octavia',
+    model: '1.6 MPI MT Active',
+    photo: carSpecial,
+    profit: 'до 300 000 ₽',
+    gift1: 'Оборудование',
+    gift2: 'КАСКО',
+    gift3: 'Комплект резины',
+    price: 'от 1 615 000 ₽',
+    credit: 'от 115 000 ₽/мес.',
+    horsePower: ' 115 л.с',
+    gasStation: '5.3 л/км',
+    speed: '189 км/ч',
+    overclocking: '10,3 c.',
+  },
+  {
+    id: 5,
+    type: 'Skoda Octavia',
+    model: '1.6 MPI MT Active',
+    photo: carSpecial,
+    profit: 'до 300 000 ₽',
+    gift1: 'Оборудование',
+    gift2: 'КАСКО',
+    gift3: 'Комплект резины',
+    price: 'от 1 615 000 ₽',
+    credit: 'от 115 000 ₽/мес.',
+    horsePower: ' 115 л.с',
+    gasStation: '5.3 л/км',
+    speed: '189 км/ч',
+    overclocking: '10,3 c.',
+  },
+  {
+    id: 6,
+    type: 'Skoda Octavia',
+    model: '1.6 MPI MT Active',
+    photo: carSpecial,
+    profit: 'до 300 000 ₽',
+    gift1: 'Оборудование',
+    gift2: 'КАСКО',
+    gift3: 'Комплект резины',
+    price: 'от 1 615 000 ₽',
+    credit: 'от 115 000 ₽/мес.',
+    horsePower: ' 115 л.с',
+    gasStation: '5.3 л/км',
+    speed: '189 км/ч',
+    overclocking: '10,3 c.',
+  },
+];
 
 export const CarAvailable = () => {
   return (
     <div className="car-with-pts-block">
       <h3 className="header-pts">Автомобили в наличии с ПТС</h3>
       <div className="main-car-container">
-        <div className="individual-car-container">
-          <div className="top-block">
-            <img id="fon" src={fonCity} alt="fon" />
-            <img id="car" src={car} alt="car" />
-            <p>
-              Skoda Octavia <br /> 1.6 MPI MT Active
-            </p>
-            <div className="top-pic">
-              <img src={whiteHeart} alt="like" />
-              <img src={layer} alt="layer" />
-            </div>
-          </div>
-          <div id="vygoda">
-            <img src={redSpace} alt="redSpace" />
-            <p id="day-sale">Предложение дня</p>
-            <p>
-              Выгода <br /> до 300 000 ₽
-            </p>
-          </div>
-          <div className="information-discount">
-            <img src={present} alt="present" />
-            <div>
-              <p className="style">Оборудование</p>
-              <p className="style gift">в подарок</p>
-            </div>
-            <img src={present} alt="present" />
-            <div>
-              <p className="style">КАСКО</p>
-              <p className="style gift">в подарок</p>
-            </div>
-            <img src={present} alt="present" />
-            <div>
-              <p className="style">
-                Комплект <br /> резины
-              </p>
-              <p className="style gift">в подарок</p>
-            </div>
-          </div>
-          <div className="price">
-            <p className="grid-a">от 1 615 000 ₽</p>
-            <p className="grid-b">
-              Кредит <b>от 115 000 ₽/мес.</b>
-            </p>
-            <img className="grid-c" src={horsePower} alt="horsePower" />
-            <img className="grid-d" src={gasStation} alt="gasStation" />
-            <img className="grid-e" src={speed} alt="speed" />
-            <img className="grid-f" src={overclocking} alt="overclocking" />
-          </div>
-          <div className="pos">
-            <p>115 л.с</p>
-            <p>5.3 л/км</p>
-            <p>189 км/ч</p>
-            <p>10,3 c.</p>
-          </div>
-          <div className="buttons">
-            <img className="red-b" src={redButton} alt="redButton" />
-            <img className="black-b" src={blackButton} alt="blackButton" />
-            <img className="grey-b" src={greyButton} alt="greyButton" />
-            <div className="button-text">
-              <button>Резерв онлайн</button>
-              <button>Купить</button>
-              <button>Подробнее</button>
-            </div>
-          </div>
-        </div>
-        <div className="individual-car-container">
-          <div className="top-block">
-            <img id="fon" src={fonCity} alt="fon" />
-            <img id="car" src={car} alt="car" />
-            <p>
-              Skoda Octavia <br /> 1.6 MPI MT Active
-            </p>
-            <div className="top-pic">
-              <img src={whiteHeart} alt="like" />
-              <img src={layer} alt="layer" />
-            </div>
-          </div>
-          <div id="vygoda">
-            <img src={redSpace} alt="redSpace" />
-            <p id="day-sale">Предложение дня</p>
-            <p>
-              Выгода <br /> до 300 000 ₽
-            </p>
-          </div>
-          <div className="information-discount">
-            <img src={present} alt="present" />
-            <div>
-              <p className="style">Оборудование</p>
-              <p className="style gift">в подарок</p>
-            </div>
-            <img src={present} alt="present" />
-            <div>
-              <p className="style">КАСКО</p>
-              <p className="style gift">в подарок</p>
-            </div>
-            <img src={present} alt="present" />
-            <div>
-              <p className="style">
-                Комплект <br /> резины
-              </p>
-              <p className="style gift">в подарок</p>
-            </div>
-          </div>
-          <div className="price">
-            <p className="grid-a">от 1 615 000 ₽</p>
-            <p className="grid-b">
-              Кредит <b>от 115 000 ₽/мес.</b>
-            </p>
-            <img className="grid-c" src={horsePower} alt="horsePower" />
-            <img className="grid-d" src={gasStation} alt="gasStation" />
-            <img className="grid-e" src={speed} alt="speed" />
-            <img className="grid-f" src={overclocking} alt="overclocking" />
-          </div>
-          <div className="pos">
-            <p>115 л.с</p>
-            <p>5.3 л/км</p>
-            <p>189 км/ч</p>
-            <p>10,3 c.</p>
-          </div>
-          <div className="buttons">
-            <img className="red-b" src={redButton} alt="redButton" />
-            <img className="black-b" src={blackButton} alt="blackButton" />
-            <img className="grey-b" src={greyButton} alt="greyButton" />
-            <div className="button-text">
-              <button>Резерв онлайн</button>
-              <button>Купить</button>
-              <button>Подробнее</button>
-            </div>
-          </div>
-        </div>
-        <div className="individual-car-container">
-          <div className="top-block">
-            <img id="fon" src={fonCity} alt="fon" />
-            <img id="car" src={car} alt="car" />
-            <p>
-              Skoda Octavia <br /> 1.6 MPI MT Active
-            </p>
-            <div className="top-pic">
-              <img src={whiteHeart} alt="like" />
-              <img src={layer} alt="layer" />
-            </div>
-          </div>
-          <div id="vygoda">
-            <img src={redSpace} alt="redSpace" />
-            <p id="day-sale">Предложение дня</p>
-            <p>
-              Выгода <br /> до 300 000 ₽
-            </p>
-          </div>
-          <div className="information-discount">
-            <img src={present} alt="present" />
-            <div>
-              <p className="style">Оборудование</p>
-              <p className="style gift">в подарок</p>
-            </div>
-            <img src={present} alt="present" />
-            <div>
-              <p className="style">КАСКО</p>
-              <p className="style gift">в подарок</p>
-            </div>
-            <img src={present} alt="present" />
-            <div>
-              <p className="style">
-                Комплект <br /> резины
-              </p>
-              <p className="style gift">в подарок</p>
-            </div>
-          </div>
-          <div className="price">
-            <p className="grid-a">от 1 615 000 ₽</p>
-            <p className="grid-b">
-              Кредит <b>от 115 000 ₽/мес.</b>
-            </p>
-            <img className="grid-c" src={horsePower} alt="horsePower" />
-            <img className="grid-d" src={gasStation} alt="gasStation" />
-            <img className="grid-e" src={speed} alt="speed" />
-            <img className="grid-f" src={overclocking} alt="overclocking" />
-          </div>
-          <div className="pos">
-            <p>115 л.с</p>
-            <p>5.3 л/км</p>
-            <p>189 км/ч</p>
-            <p>10,3 c.</p>
-          </div>
-          <div className="buttons">
-            <img className="red-b" src={redButton} alt="redButton" />
-            <img className="black-b" src={blackButton} alt="blackButton" />
-            <img className="grey-b" src={greyButton} alt="greyButton" />
-            <div className="button-text">
-              <button>Резерв онлайн</button>
-              <button>Купить</button>
-              <button>Подробнее</button>
-            </div>
-          </div>
-        </div>
-        <div className="individual-car-container">
-          <div className="top-block">
-            <img id="fon" src={fonCity} alt="fon" />
-            <img id="car" src={car} alt="car" />
-            <p>
-              Skoda Octavia <br /> 1.6 MPI MT Active
-            </p>
-            <div className="top-pic">
-              <img src={whiteHeart} alt="like" />
-              <img src={layer} alt="layer" />
-            </div>
-          </div>
-          <div id="vygoda">
-            <img src={redSpace} alt="redSpace" />
-            <p id="day-sale">Предложение дня</p>
-            <p>
-              Выгода <br /> до 300 000 ₽
-            </p>
-          </div>
-          <div className="information-discount">
-            <img src={present} alt="present" />
-            <div>
-              <p className="style">Оборудование</p>
-              <p className="style gift">в подарок</p>
-            </div>
-            <img src={present} alt="present" />
-            <div>
-              <p className="style">КАСКО</p>
-              <p className="style gift">в подарок</p>
-            </div>
-            <img src={present} alt="present" />
-            <div>
-              <p className="style">
-                Комплект <br /> резины
-              </p>
-              <p className="style gift">в подарок</p>
-            </div>
-          </div>
-          <div className="price">
-            <p className="grid-a">от 1 615 000 ₽</p>
-            <p className="grid-b">
-              Кредит <b>от 115 000 ₽/мес.</b>
-            </p>
-            <img className="grid-c" src={horsePower} alt="horsePower" />
-            <img className="grid-d" src={gasStation} alt="gasStation" />
-            <img className="grid-e" src={speed} alt="speed" />
-            <img className="grid-f" src={overclocking} alt="overclocking" />
-          </div>
-          <div className="pos">
-            <p>115 л.с</p>
-            <p>5.3 л/км</p>
-            <p>189 км/ч</p>
-            <p>10,3 c.</p>
-          </div>
-          <div className="buttons">
-            <img className="red-b" src={redButton} alt="redButton" />
-            <img className="black-b" src={blackButton} alt="blackButton" />
-            <img className="grey-b" src={greyButton} alt="greyButton" />
-            <div className="button-text">
-              <button>Резерв онлайн</button>
-              <button>Купить</button>
-              <button>Подробнее</button>
-            </div>
-          </div>
-        </div>
-        <div className="individual-car-container">
-          <div className="top-block">
-            <img id="fon" src={fonCity} alt="fon" />
-            <img id="car" src={car} alt="car" />
-            <p>
-              Skoda Octavia <br /> 1.6 MPI MT Active
-            </p>
-            <div className="top-pic">
-              <img src={whiteHeart} alt="like" />
-              <img src={layer} alt="layer" />
-            </div>
-          </div>
-          <div id="vygoda">
-            <img src={redSpace} alt="redSpace" />
-            <p id="day-sale">Предложение дня</p>
-            <p>
-              Выгода <br /> до 300 000 ₽
-            </p>
-          </div>
-          <div className="information-discount">
-            <img src={present} alt="present" />
-            <div>
-              <p className="style">Оборудование</p>
-              <p className="style gift">в подарок</p>
-            </div>
-            <img src={present} alt="present" />
-            <div>
-              <p className="style">КАСКО</p>
-              <p className="style gift">в подарок</p>
-            </div>
-            <img src={present} alt="present" />
-            <div>
-              <p className="style">
-                Комплект <br /> резины
-              </p>
-              <p className="style gift">в подарок</p>
-            </div>
-          </div>
-          <div className="price">
-            <p className="grid-a">от 1 615 000 ₽</p>
-            <p className="grid-b">
-              Кредит <b>от 115 000 ₽/мес.</b>
-            </p>
-            <img className="grid-c" src={horsePower} alt="horsePower" />
-            <img className="grid-d" src={gasStation} alt="gasStation" />
-            <img className="grid-e" src={speed} alt="speed" />
-            <img className="grid-f" src={overclocking} alt="overclocking" />
-          </div>
-          <div className="pos">
-            <p>115 л.с</p>
-            <p>5.3 л/км</p>
-            <p>189 км/ч</p>
-            <p>10,3 c.</p>
-          </div>
-          <div className="buttons">
-            <img className="red-b" src={redButton} alt="redButton" />
-            <img className="black-b" src={blackButton} alt="blackButton" />
-            <img className="grey-b" src={greyButton} alt="greyButton" />
-            <div className="button-text">
-              <button>Резерв онлайн</button>
-              <button>Купить</button>
-              <button>Подробнее</button>
-            </div>
-          </div>
-        </div>
-        <div className="individual-car-container">
-          <div className="top-block">
-            <img id="fon" src={fonCity} alt="fon" />
-            <img id="car" src={car} alt="car" />
-            <p>
-              Skoda Octavia <br /> 1.6 MPI MT Active
-            </p>
-            <div className="top-pic">
-              <img src={whiteHeart} alt="like" />
-              <img src={layer} alt="layer" />
-            </div>
-          </div>
-          <div id="vygoda">
-            <img src={redSpace} alt="redSpace" />
-            <p id="day-sale">Предложение дня</p>
-            <p>
-              Выгода <br /> до 300 000 ₽
-            </p>
-          </div>
-          <div className="information-discount">
-            <img src={present} alt="present" />
-            <div>
-              <p className="style">Оборудование</p>
-              <p className="style gift">в подарок</p>
-            </div>
-            <img src={present} alt="present" />
-            <div>
-              <p className="style">КАСКО</p>
-              <p className="style gift">в подарок</p>
-            </div>
-            <img src={present} alt="present" />
-            <div>
-              <p className="style">
-                Комплект <br /> резины
-              </p>
-              <p className="style gift">в подарок</p>
-            </div>
-          </div>
-          <div className="price">
-            <p className="grid-a">от 1 615 000 ₽</p>
-            <p className="grid-b">
-              Кредит <b>от 115 000 ₽/мес.</b>
-            </p>
-            <img className="grid-c" src={horsePower} alt="horsePower" />
-            <img className="grid-d" src={gasStation} alt="gasStation" />
-            <img className="grid-e" src={speed} alt="speed" />
-            <img className="grid-f" src={overclocking} alt="overclocking" />
-          </div>
-          <div className="pos">
-            <p>115 л.с</p>
-            <p>5.3 л/км</p>
-            <p>189 км/ч</p>
-            <p>10,3 c.</p>
-          </div>
-          <div className="buttons">
-            <img className="red-b" src={redButton} alt="redButton" />
-            <img className="black-b" src={blackButton} alt="blackButton" />
-            <img className="grey-b" src={greyButton} alt="greyButton" />
-            <div className="button-text">
-              <button>Резерв онлайн</button>
-              <button>Купить</button>
-              <button>Подробнее</button>
-            </div>
-          </div>
-        </div>
+        {carAvailable.map((item) => (
+          <IndividualCarContainer
+            id={item.id}
+            type={item.type}
+            model={item.model}
+            photo={item.photo}
+            profit={item.profit}
+            gift1={item.gift1}
+            gift2={item.gift2}
+            gift3={item.gift3}
+            price={item.price}
+            credit={item.credit}
+            horsePower={item.horsePower}
+            gasStation={item.gasStation}
+            speed={item.speed}
+            overclocking={item.overclocking}
+          />
+        ))}
       </div>
     </div>
   );
---
+++ src/components/IndividualCarContainer/IndividualCarContainer.tsx
@@ -0,0 +1,101 @@
+import './IndividualCarContainer.scss';
+import React from 'react';
+import blackButton from './assets/black-button.svg';
+import fonCity from './assets/fon-city.png';
+import gasStation from './assets/gas-station.png';
+import greyButton from './assets/grey-button.svg';
+import horsePower from './assets/horse-power.png';
+import layer from './assets/layer.png';
+import overclocking from './assets/overclocking.png';
+import present from './assets/present.svg';
+import redButton from './assets/red-button.svg';
+import redHeart from './assets/red-heart.png';
+import redSpace from './assets/red-space.svg';
+import speed from './assets/speed.png';
+import whiteHeart from './assets/white-heart.png';
+
+interface IndividualCarContainerProps {
+  id: number;
+  model: string;
+  type: string;
+  photo: string;
+  profit: string;
+  gift1: string;
+  gift2: string;
+  gift3: string;
+  price: string;
+  credit: string;
+  horsePower: string;
+  gasStation: string;
+  speed: string;
+  overclocking: string;
+}
+
+export const IndividualCarContainer = (props: IndividualCarContainerProps) => {
+  return (
+    <div className="individual-car-container">
+      <div className="top-block">
+        <img id="fon" src={fonCity} alt="fon" />
+        <img id="car" src={props.photo} alt="carSpecial" />
+        <p>
+          Skoda Octavia <p>{props.model}</p>
+        </p>
+        <div className="top-pic">
+          <img src={whiteHeart} alt="like" />
+          <img src={layer} alt="layer" />
+        </div>
+      </div>
+      <div id="vygoda">
+        <img src={redSpace} alt="redSpace" />
+        <p id="day-sale">Предложение дня</p>
+        <div>
+          <p>Выгода</p>
+          <p>{props.profit}</p>
+        </div>
+      </div>
+      <div className="information-discount">
+        <img src={present} alt="present" />
+        <p className="style">
+          {props.gift1}
+          <p className="style gift">в подарок</p>
+        </p>
+        <img src={present} alt="present" />
+        <p className="style">
+          {props.gift2}
+          <p className="style gift">в подарок</p>
+        </p>
+        <img src={present} alt="present" />
+        <p className="style">
+          {props.gift3}
+          <p className="style gift">в подарок</p>
+        </p>
+      </div>
+      <div className="price">
+        <p className="grid-a">{props.price}</p>
+        <p className="grid-b">
+          Кредит <b>{props.credit}</b>
+        </p>
+        <img className="grid-c" src={horsePower} alt="horsePower" />
+        <img className="grid-d" src={gasStation} alt="gasStation" />
+        <img className="grid-e" src={speed} alt="speed" />
+        <img className="grid-f" src={overclocking} alt="overclocking" />
+      </div>
+      <div className="pos">
+        <p>{props.horsePower}</p>
+        <p>{props.gasStation}</p>
+        <p>{props.speed}</p>
+        <p>{props.overclocking}</p>
+      </div>
+      <div className="buttons">
+        <img className="red-b" src={redButton} alt="redButton" />
+        <img className="black-b" src={blackButton} alt="blackButton" />
+        <img className="grey-b" src={greyButton} alt="greyButton" />
+        <div className="button-text">
+          <button>Резерв онлайн</button>
+          <button>Купить</button>
+          <button>Подробнее</button>
+        </div>
+      </div>
+    </div>
+  );
+};
