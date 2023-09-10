442e6a813490fb523a07b8453290b2e2bbfce72e
Eugene <kulinich.eugene.2412@gmail.com>
2023-09-11 01:06:27 +0600

    add reviews block

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -3,11 +3,13 @@ import { AboutCompany } from '../AboutCompanyPage/AboutCompanyPage';
 import { AboutTabControl } from './components/AboutTabControl/AboutTabControl';
 import { Location } from '../LocationPage/LocationPage';
 import { QuickSelectionPage } from '../QuickSelectionPage/QuickSelectionPage';
+import { ReviewsSection } from '../ReviewsPage/components/ReviewsSection/ReviewsSection';
 
 export const HomePage = () => {
   return (
     <>
       <QuickSelectionPage />
+      <ReviewsSection />
       <AboutCompany />
       <AboutTabControl />
       <Location />
--- src/pages/ReviewsPage/ReviewsPage.tsx
+++ src/pages/ReviewsPage/ReviewsPage.tsx
@@ -1,5 +1,18 @@
+import '../../index.scss';
 import './ReviewsPage.scss';
+import { ReviewsSection } from './components/ReviewsSection/ReviewsSection';
+import { reviews } from './models';
+import YouTube, { YouTubeProps } from 'react-youtube';
 
 export const ReviewsPage = () => {
-  return <div>ReviewsPage</div>;
+  return (
+    <>
+      <ReviewsSection />
+      <section>
+        <div className="button-block">
+          <button className="btn">Показать еще</button>
+        </div>
+      </section>
+    </>
+  );
 };
