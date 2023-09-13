11ff9b32c542371628180b36bac21f95158eb1b2
DariaKostrik <dolorez0927@yandex.ru>
2023-09-14 02:32:55 +0300

    remove comments

--- src/components/CarAvailable/CarAvailable.tsx
+++ src/components/CarAvailable/CarAvailable.tsx
@@ -5,23 +5,6 @@ import { IndividualCarContainer } from '../IndividualCarContainer/IndividualCarC
 import React from 'react';
 import carSpecial from './assets/car.png';
 
-// export interface Car {
-//   id: number;
-//   model: string;
-//   type: string;
-//   photo: string;
-//   profit: string;
-//   gift1: string;
-//   gift2: string;
-//   gift3: string;
-//   price: string;
-//   credit: string;
-//   horsePower: string;
-//   gasStation: string;
-//   speed: string;
-//   overclocking: string;
-// }
-
 const carAvailable: Car[] = [
   {
     id: 1,
