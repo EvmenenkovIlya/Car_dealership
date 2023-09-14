819f658445c5b3d61eab212b137a277d501fb3ac
DariaKostrik <dolorez0927@yandex.ru>
2023-09-14 18:57:21 +0300

    checl

--- src/pages/HomePage/components/Сarousel/Сarousel.tsx
+++ src/pages/HomePage/components/Сarousel/Сarousel.tsx
@@ -1,14 +1,20 @@
 import './Сarousel.scss';
 import React from 'react';
-import background from './components/background.png';
+import background from './assets/background.png';
+import carPhoto from './assets/ad-car.png';
 
 export const Сarousel = () => {
   return (
     <div className="basic-carousel">
       <img id="back" src={background} alt="background" />
-      <div className='full-ad'>
-      <p id="advertisement">Грандиозная распродажа <br/>тестового парка!</p>
-
+      <div className="full-ad">
+        <p id="left-only">Осталось всего 10 авто!</p>
+        <p id="advertisement">
+          Грандиозная распродажа тестового парка!
+        </p>
+      </div>
+      <div className="picture-with-ad">
+        <img className="pic-ad" src={carPhoto} alt="carPhoto" />
       </div>
     </div>
   );
