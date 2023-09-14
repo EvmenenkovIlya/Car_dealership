702178d0a950a9f29ce4a1ecc26210097d2f8d46
DariaKostrik <dolorez0927@yandex.ru>
2023-09-14 19:06:08 +0300

    add-carousel

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,12 +1,14 @@
 import './HomePage.scss';
 import { AboutCompany } from '../AboutCompanyPage/AboutCompanyPage';
 import { AboutTabControl } from './components/AboutTabControl/AboutTabControl';
+import { Carousel } from './components/Carousel/Carousel';
 import { Location } from '../LocationPage/LocationPage';
 import { QuickSelectionPage } from '../QuickSelectionPage/QuickSelectionPage';
 
 export const HomePage = () => {
   return (
     <>
+      <Carousel />
       <QuickSelectionPage />
       <AboutCompany />
       <AboutTabControl />
---
+++ src/pages/HomePage/components/Carousel/Carousel.tsx
@@ -0,0 +1,19 @@
+import './Carousel.scss';
+import React from 'react';
+import background from './assets/background.png';
+import carPhoto from './assets/ad-car.png';
+
+export const Carousel = () => {
+  return (
+    <div className="basic-carousel">
+      <img id="back" src={background} alt="background" />
+      <div className="full-ad">
+        <p id="left-only">Осталось всего 10 авто!</p>
+        <p id="advertisement">Грандиозная распродажа тестового парка!</p>
+      </div>
+      <div className="picture-with-ad">
+        <img className="pic-ad" src={carPhoto} alt="carPhoto" />
+      </div>
+    </div>
+  );
+};
