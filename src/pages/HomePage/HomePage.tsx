99039104551656b71d62db83f63765c94f2adfbc
Eugene <kulinich.eugene.2412@gmail.com>
2023-09-14 23:07:06 +0600

    add example

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -2,11 +2,22 @@ import './HomePage.scss';
 import { AboutCompany } from '../AboutCompanyPage/AboutCompanyPage';
 import { AboutTabControl } from './components/AboutTabControl/AboutTabControl';
 import { Location } from '../LocationPage/LocationPage';
+import { NavLink } from 'react-router-dom';
 import { QuickSelectionPage } from '../QuickSelectionPage/QuickSelectionPage';
+import useBreadcrumbs from 'use-react-router-breadcrumbs';
+
+const routes = [{ path: '', breadcrumb: 'Главная' }];
 
 export const HomePage = () => {
+  const breadcrumbs = useBreadcrumbs(routes);
+
   return (
     <>
+      {breadcrumbs.map(({ match, breadcrumb }) => (
+        <NavLink key={match.pathname} to={match.pathname}>
+          {breadcrumb}
+        </NavLink>
+      ))}
       <QuickSelectionPage />
       <AboutCompany />
       <AboutTabControl />
