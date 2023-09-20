fa49aa482c8158d63f5399fae8d5526d6645a260
Eugene <kulinich.eugene.2412@gmail.com>
2023-09-21 01:36:51 +0600

    add partners

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -3,6 +3,7 @@ import { AboutCompany } from '../AboutCompanyPage/AboutCompanyPage';
 import { AboutTabControl } from './components/AboutTabControl/AboutTabControl';
 import { Blog } from '../BlogPage/BlogPage';
 import { Location } from '../LocationPage/LocationPage';
+import { Partners } from '../../components/Partners/Partners';
 import { Promotion } from '../../components/Promotion/Promotion';
 import { QuickSelectionPage } from '../QuickSelectionPage/QuickSelectionPage';
 import { ReviewsSection } from '../ReviewsPage/components/ReviewsSection/ReviewsSection';
@@ -13,6 +14,7 @@ export const HomePage = () => {
     <>
       <Carousel />
       <QuickSelectionPage />
+      <Partners />
       <ReviewsSection />
       <AboutCompany />
       <AboutTabControl />
